1b4d992d22473aa462fdf83e5ca7610f27b8545b
Vlad <44032620+d0m1nus@users.noreply.github.com>
2019-04-22 19:59:44 +0700

    Create 2.cpp

---
+++ practical work 17/2.cpp
@@ -0,0 +1,118 @@
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <clocale>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+enum topic_num {
+	scientif, 
+	popular,
+	popularscience, 
+	children
+};
+struct journal {
+	char title[20];
+	float cost;
+	int number;
+	topic_num topic;
+};
+
+void print_journal(journal *buf) {
+	cout.setf(ios::left);
+	cout << "\n  " << setw(20) << buf->title;
+	cout << setw(10) << buf->cost << setw(5) << buf->number;
+	if (buf->topic == 0) {
+		cout << "научный" << endl;
+	}
+	else if (buf->topic == 1) {
+		cout << "популярный" << endl;
+	}
+	else if (buf->topic == 2) {
+		cout << "научно-популярный" << endl;
+	}
+	else {
+		cout << "детский" << endl;
+	}
+}
+
+void sort(vector<journal> &buf, int n) {
+	journal temp;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (strcmp(buf[i].title, buf[j].title) < 0) {
+				temp = buf[i];
+				buf[i] = buf[j];
+				buf[j] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	ifstream fin("journals.txt");
+
+	char filename[255];
+	cout << "Введеите имя файла: ";
+	cin.getline(filename, 255);
+
+	fstream fout(filename, ios::binary | ios::in);
+	ifstream in(filename, ios::binary | ios::out);
+
+	if (!fin.is_open()) {
+		cout << "Не удалось открыть файл!" << endl;
+		return -1;
+	}
+
+	vector<journal> buf;
+
+	int n = 0;
+	//заполнение массива структур
+	cout << "\nДанные о журналах из текстового файла: " << endl;
+	journal buffer;
+	while (fin >> buffer.title) {
+		fin >> buffer.cost >> buffer.number;
+		char buf_topic[20];
+		fin >> buf_topic;
+		if (strcmp(buf_topic, "научный") == 0) {
+			buffer.topic = scientif;
+		}
+		else if (strcmp(buf_topic, "популярный") == 0) {
+			buffer.topic = popular;
+		}
+		else if (strcmp(buf_topic, "научно-популярный") == 0) {
+			buffer.topic = popularscience;
+		}
+		else {
+			buffer.topic = children;
+		}
+		buf.push_back(buffer);
+		print_journal(&buffer);
+		n++;
+
+	}
+
+	sort(buf, n);//сортировка массива структур по алфавиту
+	
+	//запись в двоичный файл сортированного списка
+	fout.seekp(ios::beg);
+	fout.write((char *)&buf[0], sizeof(buf[0])&n);
+
+	//вывод списка из двоичного файла
+	cout << "\nДанные о журналах из двоичного файла: " << endl;
+	fout.seekg(0, ios_base::beg);
+	
+	for (int i = 0; i < n; i++) {		
+		in.read((char *)&buffer, sizeof(buffer));
+		print_journal(&buffer);
+	}
+
+	fin.close();
+	fout.close();
+	
+	return 0;
+}
